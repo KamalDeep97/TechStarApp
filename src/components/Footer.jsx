57c90ac04d4f3a4c5e8f6802a57500202737947a
Kamal Deep <kamaldeepkumar97@gmail.com>
2023-07-13 14:29:13 +0530

    First Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return <footer>
+    <div>
+        <h1>TechStar</h1>
+        <p>@all right reserved</p>
+    </div>
+
+    <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href='https://www.google.com/'>Google</a>
+            <a href='https://www.youtube.com/'>Youtube</a>
+            <a href='https://github.com/'>Github</a>
+        </div>
+    </div>
+  </footer>
+}
+
+export default Footer;
